1554e165f7d263b511df5ce411426ddc33e7bc83
Nicolas Boutet <amd3002@gmail.com>
2023-04-28 10:48:35 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -7,22 +7,29 @@ import json
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('name', [
-  ('influxdb2'),
-  ('influxdb2-cli'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("influxdb2"),
+        ("influxdb2-cli"),
+    ],
+)
 def test_packages_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
 
 
-@pytest.mark.parametrize('username,groupname,path', [
-  ('influxdb', 'influxdb', '/etc/influxdb/config.yml'),
-  ('root', 'root', '/etc/default/influxdb2'),
-])
+@pytest.mark.parametrize(
+    "username,groupname,path",
+    [
+        ("influxdb", "influxdb", "/etc/influxdb/config.yml"),
+        ("root", "root", "/etc/default/influxdb2"),
+    ],
+)
 def test_influxdb_config_file(host, username, groupname, path):
     config = host.file(path)
     assert config.exists
@@ -31,9 +38,12 @@ def test_influxdb_config_file(host, username, groupname, path):
     assert config.group == groupname
 
 
-@pytest.mark.parametrize('username,groupname,path', [
-  ('root', 'root', '/lib/systemd/system/influxdb.service'),
-])
+@pytest.mark.parametrize(
+    "username,groupname,path",
+    [
+        ("root", "root", "/lib/systemd/system/influxdb.service"),
+    ],
+)
 def test_systemd_config_file_exists(host, username, groupname, path):
     config = host.file(path)
     assert config.exists
@@ -42,9 +52,12 @@ def test_systemd_config_file_exists(host, username, groupname, path):
     assert config.group == groupname
 
 
-@pytest.mark.parametrize('name', [
-  ('influxdb'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("influxdb"),
+    ],
+)
 def test_influxdb_service_is_running_and_enabled(host, name):
     service = host.service(name)
     assert service.is_enabled
@@ -52,15 +65,23 @@ def test_influxdb_service_is_running_and_enabled(host, name):
 
 
 def test_influxdb_replies_to_ping(host):
-    command = 'influx ping'
+    command = "influx ping"
     ping = host.check_output(command)
-    assert ping == 'OK'
+    assert ping == "OK"
 
 
-@pytest.mark.parametrize('url,token,org,active,path', [
-  (os.environ['INFLUX_HOST'], 'EXAMPLE-TOKEN', 'example-org',
-   'true', '/root/.influxdbv2/configs'),
-])
+@pytest.mark.parametrize(
+    "url,token,org,active,path",
+    [
+        (
+            os.environ["INFLUX_HOST"],
+            "EXAMPLE-TOKEN",
+            "example-org",
+            "true",
+            "/root/.influxdbv2/configs",
+        ),
+    ],
+)
 def test_influxdb_auth_file(host, url, token, org, active, path):
     config = host.file(path)
     assert config.exists
@@ -68,56 +89,65 @@ def test_influxdb_auth_file(host, url, token, org, active, path):
     assert config.contains(f'url = "{url}"')
     assert config.contains(f'token = "{token}"')
     assert config.contains(f'org = "{org}"')
-    assert config.contains(f'active = {active}')
+    assert config.contains(f"active = {active}")
 
 
-@pytest.mark.parametrize('name,description', [
-  ('example-org', ''),
-  ('main-org', 'Main organization'),
-  ('guest-org', ''),
-])
+@pytest.mark.parametrize(
+    "name,description",
+    [
+        ("example-org", ""),
+        ("main-org", "Main organization"),
+        ("guest-org", ""),
+    ],
+)
 def test_influxdb_organizations_exist(host, name, description):
-    command = 'influx org ls --json'
+    command = "influx org ls --json"
     json_data = host.check_output(command)
     items_list = json.loads(json_data)
     for item in items_list:
-        if item['name'] == name and item['description'] == description:
+        if item["name"] == name and item["description"] == description:
             assert True
             break
     else:
         assert False
 
 
-@pytest.mark.parametrize('name', [
-  ('example-user'),
-  ('admin01'),
-  ('guest01'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("example-user"),
+        ("admin01"),
+        ("guest01"),
+    ],
+)
 def test_influxdb_users_exist(host, name):
-    command = 'influx user ls --json'
+    command = "influx user ls --json"
     json_data = host.check_output(command)
     items_list = json.loads(json_data)
     for item in items_list:
-        if item['name'] == name:
+        if item["name"] == name:
             assert True
             break
     else:
         assert False
 
 
-@pytest.mark.parametrize('name,description,org', [
-  ('example-bucket', '', 'example-org'),
-  ('bucket01', 'First bucket', 'main-org'),
-  ('bucket02', '', 'main-org'),
-])
+@pytest.mark.parametrize(
+    "name,description,org",
+    [
+        ("example-bucket", "", "example-org"),
+        ("bucket01", "First bucket", "main-org"),
+        ("bucket02", "", "main-org"),
+    ],
+)
 def test_influxdb_buckets_exist(host, name, description, org):
-    command = f'influx bucket ls --json --org {org}'
+    command = f"influx bucket ls --json --org {org}"
     json_data = host.check_output(command)
     items_list = json.loads(json_data)
     for item in items_list:
-        if item['name'] == name:
-            if 'description' in item:
-                assert item['description'] == description
+        if item["name"] == name:
+            if "description" in item:
+                assert item["description"] == description
                 break
             assert True
             break
